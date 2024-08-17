32382b945d8ac854966ad7dbcb2f2f6170e3a451
RAJWARDHAN-B <147420707+RAJWARDHAN-B@users.noreply.github.com>
2024-08-17 08:46:04 +0530

    draw condition added

--- index.js
+++ index.js
@@ -11,4 +11,6 @@ if (randomNumber1 > randomNumber2){
     document.querySelector("h1").textContent = "🚩Player 1 Wins !";
 }else if (randomNumber2 > randomNumber1){
     document.querySelector("h1").textContent = "🚩Player 2 Wins !";
+}else{
+    document.querySelector("h1").textContent = "🏳️ Draw !"
 }
